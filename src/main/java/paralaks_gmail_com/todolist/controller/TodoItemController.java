b10a2bb8df1c6edbb97c2b49401355ac656469f9
Murat Kurt <paralaks@gmail.com>
2021-02-27 15:06:19 -0500

    Refactored rest endpoints to respond to correct verbs + http statuses

--- src/main/java/paralaks_gmail_com/todolist/controller/TodoItemController.java
+++ src/main/java/paralaks_gmail_com/todolist/controller/TodoItemController.java
@@ -1,11 +1,14 @@
 package paralaks_gmail_com.todolist.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import paralaks_gmail_com.todolist.model.TodoItem;
 import paralaks_gmail_com.todolist.service.TodoItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/todoitems")
@@ -17,32 +20,45 @@ public class TodoItemController {
     todoItemService = service;
   }
 
-  @RequestMapping("/")
-  public List<TodoItem> list() {
-    List<TodoItem> list = todoItemService.list(null);
-    list.forEach(l -> l.setListId(l.getTodoList().getId()));
-    return list;
+  private ResponseEntity<List<TodoItem>> getTodoItemListResponse(Integer listId) {
+    Optional<List<TodoItem>> list = todoItemService.list(listId);
+
+    return list.isPresent() ? ResponseEntity.ok(list.get()) : ResponseEntity.notFound().build();
   }
 
-  @RequestMapping("/listId/{listId}")
-  public List<TodoItem> listFor(@PathVariable Integer listId) {
-    List<TodoItem> list = todoItemService.list(listId);
-    list.forEach(l -> l.setListId(listId));
-    return list;
+  @GetMapping("/")
+  public ResponseEntity<List<TodoItem>> list() {
+    return getTodoItemListResponse(null);
   }
 
-  @RequestMapping("/{id}")
-  public TodoItem todoItem(@PathVariable Integer id) {
-    return todoItemService.findOne(id).orElse(null);
+  @GetMapping("/listId/{listId}")
+  public ResponseEntity<List<TodoItem>> listFor(@PathVariable Integer listId) {
+    return getTodoItemListResponse(listId);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<TodoItem> todoItem(@PathVariable Integer id) {
+    Optional<TodoItem> todoItem = todoItemService.findOne(id);
+
+    return todoItem.isPresent() ? ResponseEntity.ok(todoItem.get()) : ResponseEntity.notFound().build();
   }
 
   @PostMapping("/add")
-  public TodoItem add(@RequestBody TodoItem item) {
-    return todoItemService.addOne(item);
+  public ResponseEntity<TodoItem> add(@RequestBody TodoItem item) {
+    try {
+      return ResponseEntity.status(HttpStatus.CREATED).body(todoItemService.addOne(item));
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().build();
+    }
   }
 
   @DeleteMapping("/{id}")
-  public String delete(@PathVariable Integer id) {
-    return todoItemService.deleteOne(id);
+  public ResponseEntity<String> delete(@PathVariable Integer id) {
+    if (id == null)
+      ResponseEntity.notFound().build();
+
+    String result = todoItemService.deleteOne(id);
+
+    return result == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(result);
   }
 }
--- src/main/java/paralaks_gmail_com/todolist/controller/TodoListController.java
+++ src/main/java/paralaks_gmail_com/todolist/controller/TodoListController.java
@@ -1,11 +1,13 @@
 package paralaks_gmail_com.todolist.controller;
 
+import org.springframework.http.ResponseEntity;
 import paralaks_gmail_com.todolist.model.TodoList;
 import paralaks_gmail_com.todolist.service.TodoListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/todolists")
@@ -17,14 +19,15 @@ public class TodoListController {
     todoListService = service;
   }
 
-  @RequestMapping("/")
-  public List<TodoList> list() {
-    return todoListService.list();
+  @GetMapping("/")
+  public ResponseEntity<List<TodoList>> list() {
+    return ResponseEntity.ok(todoListService.list());
   }
 
-  @RequestMapping("/{id}")
-  public TodoList findOne(@PathVariable Integer id) {
-    return todoListService.findOne(id).orElse(null);
+  @GetMapping("/{id}")
+  public ResponseEntity<TodoList> findOne(@PathVariable Integer id) {
+    Optional<TodoList> list = todoListService.findOne(id);
+    return list.isPresent() ? ResponseEntity.ok(list.get()) : ResponseEntity.notFound().build();
   }
 
   @PostMapping("/add")
@@ -33,7 +36,12 @@ public class TodoListController {
   }
 
   @DeleteMapping("/{id}")
-  public String deleteOne(@PathVariable Integer id) {
-    return todoListService.deleteOne(id);
+  public ResponseEntity<String> deleteOne(@PathVariable Integer id) {
+    if (id == null)
+      return ResponseEntity.notFound().build();
+
+    String result = todoListService.deleteOne(id);
+
+    return result == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(result);
   }
 }
